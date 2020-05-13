2cbcf6f7ea65e3df25fc259af42f9ba1344197d4
dmitrytishchenko <tdmitiu@mail.ru>
2020-05-13 21:04:46 +0300

    2. Scriplet. Динамическая таблица. [#276520]

---
+++ src/main/java/ru/iob4j/dream/store/Store.java
@@ -0,0 +1,26 @@
+package ru.iob4j.dream.store;
+
+import ru.iob4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Store {
+    private static final Store INST = new Store();
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job", "description junior", new Date()));
+        posts.put(2, new Post(2, "Middle Java Job", "description middle", new Date()));
+        posts.put(3, new Post(3, "Senior Java Job", "description senior", new Date()));
+    }
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
